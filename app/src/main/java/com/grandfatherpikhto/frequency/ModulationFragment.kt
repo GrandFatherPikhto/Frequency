acc256cb132db8fb8eea66fa578d72742a1650a3
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2022-03-21 14:55:07 +0300

    Добавлено сохранение состояния и частоты и их восстановление при их запуске

--- app/src/main/java/com/grandfatherpikhto/frequency/ModulationFragment.kt
+++ app/src/main/java/com/grandfatherpikhto/frequency/ModulationFragment.kt
@@ -1,13 +1,14 @@
 package com.grandfatherpikhto.frequency
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.grandfatherpikhto.frequency.databinding.FragmentModulationBinding
 import kotlinx.coroutines.launch
@@ -17,18 +18,24 @@ import kotlin.math.roundToInt
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ModulationFragment : Fragment() {
-
-    private var _binding: FragmentModulationBinding? = null
-
     companion object {
         const val TAG:String = "ModulationFragment"
+        const val NAME:String = "ModulationFragment"
+        const val FREQUENCY:String = "Frequency"
+        const val ENABLE:String = "Enable"
     }
+
+    private var _binding: FragmentModulationBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     private val modulationModel by activityViewModels<ModulationModel>()
+
+    private val preferences by lazy {
+        requireContext().getSharedPreferences(NAME, Context.MODE_PRIVATE)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -58,17 +65,16 @@ class ModulationFragment : Fragment() {
         lifecycleScope.launch {
             modulationModel.enable.collect { enable ->
                 Log.e(TAG, "Enable: $enable")
-                if (enable) {
-                    binding.ibPlay.setImageResource(R.drawable.ic_baseline_pause_48)
-                } else {
-                    binding.ibPlay.setImageResource(R.drawable.ic_baseline_play_arrow_48)
-                }
+                setPlayIcon(enable)
             }
         }
 
         lifecycleScope.launch {
             modulationModel.frequency.collect { frequency ->
                 binding.tvPulseFrequency.text = getString(R.string.tvPulseFrequency, frequency)
+                if(binding.sliderPulseFrequency.value.roundToInt() != frequency) {
+                    binding.sliderPulseFrequency.value = frequency.toFloat()
+                }
             }
         }
     }
@@ -80,9 +86,29 @@ class ModulationFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+        // play(modulationModel.enable.value)
+        preferences.let {
+            val frequency = it.getInt(FREQUENCY, ModulationModel.DEFAULT_FREQUENCY)
+            modulationModel.changeFrequency(frequency)
+            val enable = it.getBoolean(ENABLE, ModulationModel.DEFAULT_ENABLE)
+            modulationModel.changeEnable(enable)
+        }
     }
 
     override fun onPause() {
+        preferences.edit {
+            putInt(FREQUENCY, modulationModel.frequency.value)
+            putBoolean(ENABLE, modulationModel.enable.value)
+            commit()
+        }
         super.onPause()
+    }
+
+    private fun setPlayIcon(enable: Boolean) {
+        if (enable) {
+            binding.ibPlay.setImageResource(R.drawable.ic_baseline_pause_48)
+        } else {
+            binding.ibPlay.setImageResource(R.drawable.ic_baseline_play_arrow_48)
+        }
     }
 }
