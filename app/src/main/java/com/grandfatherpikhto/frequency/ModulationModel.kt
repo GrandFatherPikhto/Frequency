f0a917cbb8f19073523351d7e4e2b586dc788339
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2022-03-21 13:29:19 +0300

    Модель, общая для MainActivity.kt и ModulationModel.kt

--- app/src/main/java/com/grandfatherpikhto/frequency/ModulationModel.kt
+++ app/src/main/java/com/grandfatherpikhto/frequency/ModulationModel.kt
@@ -1,27 +1,56 @@
 package com.grandfatherpikhto.frequency
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class ModulationModel: ViewModel() {
     companion object {
         const val TAG:String = "ModulationModel"
     }
 
-    private val sharedEnable = MutableLiveData(false)
-    val enable:LiveData<Boolean>
-        get() = sharedEnable
+    private val sharedEnable: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val enable get() = sharedEnable.asStateFlow()
 
-    private val sharedFrequency = MutableLiveData(20)
-    val frequency:LiveData<Int>
-        get() = sharedFrequency
+    private val sharedFrequency: MutableStateFlow<Int> = MutableStateFlow(20)
+    val frequency get() = sharedFrequency.asStateFlow()
 
     fun changeEnable(value:Boolean) {
-        sharedEnable.postValue(value)
+        sharedEnable.tryEmit(value)
+    }
+
+    fun resetPlay() {
+        sharedEnable.value?.let { value ->
+            sharedEnable.tryEmit(value.not())
+        }
     }
 
     fun changeFrequency(value: Int) {
-        sharedFrequency.postValue(value)
+        sharedFrequency.tryEmit(value)
+    }
+
+    init {
+        viewModelScope.launch {
+            sharedEnable.collect { value ->
+                Log.e(TAG, "Enable: $value")
+                if(value) {
+                    PlayService.play()
+                } else {
+                    PlayService.stop()
+                }
+            }
+        }
+
+        viewModelScope.launch {
+            sharedFrequency.collect { value ->
+                PlayService.frequency = value
+            }
+        }
     }
 }
