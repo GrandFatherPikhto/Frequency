bffe8ccf2ca11f6697ff21a7d1962b21274aa7a7
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2022-03-21 14:55:49 +0300

    Добавленое SavedStateHandle

--- app/src/main/java/com/grandfatherpikhto/frequency/ModulationModel.kt
+++ app/src/main/java/com/grandfatherpikhto/frequency/ModulationModel.kt
@@ -1,28 +1,38 @@
 package com.grandfatherpikhto.frequency
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ModulationModel: ViewModel() {
+@HiltViewModel
+class ModulationModel
+    @Inject internal constructor(private val savedStateHandle: SavedStateHandle): ViewModel() {
     companion object {
         const val TAG:String = "ModulationModel"
+        const val SAVED_FREQUENCY_KEY = "SavedFrequency"
+        const val DEFAULT_FREQUENCY = 20
+        const val SAVED_ENABLE_KEY = "SavedEnable"
+        const val DEFAULT_ENABLE = false
     }
 
-    private val sharedEnable: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    private val sharedEnable: MutableStateFlow<Boolean>
+        = MutableStateFlow(savedStateHandle.get(SAVED_ENABLE_KEY) ?: DEFAULT_ENABLE)
     val enable get() = sharedEnable.asStateFlow()
 
-    private val sharedFrequency: MutableStateFlow<Int> = MutableStateFlow(20)
+    private val sharedFrequency: MutableStateFlow<Int>
+        = MutableStateFlow(savedStateHandle.get(SAVED_FREQUENCY_KEY) ?: DEFAULT_FREQUENCY)
     val frequency get() = sharedFrequency.asStateFlow()
 
     fun changeEnable(value:Boolean) {
-        sharedEnable.tryEmit(value)
+        Log.e(TAG, "Enable: $value")
+        if(enable.value != value) {
+            sharedEnable.tryEmit(value)
+        }
     }
 
     fun resetPlay() {
@@ -32,13 +42,23 @@ class ModulationModel: ViewModel() {
     }
 
     fun changeFrequency(value: Int) {
-        sharedFrequency.tryEmit(value)
+        Log.e(TAG, "Frequency: $value")
+        if(frequency.value != value) {
+            sharedFrequency.tryEmit(value)
+        }
+    }
+
+    override fun onCleared() {
+        savedStateHandle.set(SAVED_FREQUENCY_KEY, frequency.value)
+        super.onCleared()
     }
 
     init {
+        Log.e(TAG, "frequency: ${frequency.value}")
         viewModelScope.launch {
             sharedEnable.collect { value ->
                 Log.e(TAG, "Enable: $value")
+                savedStateHandle.set(SAVED_ENABLE_KEY, value)
                 if(value) {
                     PlayService.play()
                 } else {
@@ -50,6 +70,9 @@ class ModulationModel: ViewModel() {
         viewModelScope.launch {
             sharedFrequency.collect { value ->
                 PlayService.frequency = value
+                savedStateHandle.set(SAVED_FREQUENCY_KEY, frequency.value)
+                val freq = savedStateHandle.get(SAVED_FREQUENCY_KEY) ?: DEFAULT_FREQUENCY
+                Log.e(TAG, "Frequency: $freq")
             }
         }
     }
