ca58bbebc4a9c54ff097e9fd5371155d6c6dd3fd
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2022-03-22 16:23:55 +0300

    Добавлено окошко уведомлений с кнопкой "Стоп"

--- app/src/main/java/com/grandfatherpikhto/frequency/PlayService.kt
+++ app/src/main/java/com/grandfatherpikhto/frequency/PlayService.kt
@@ -2,6 +2,7 @@ package com.grandfatherpikhto.frequency
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -11,27 +12,32 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import kotlin.math.roundToInt
+import kotlin.properties.Delegates
 
 class PlayService: Service() {
     companion object {
         const val TAG:String = "PlayService"
+        const val ONGOING_NOTIFICATION_ID = 1
+        const val EXTRA_NOTIFICATION_ID = "stopService"
+        const val ACTION_STOP = "com.rqd.Frequency.ACTION_STOP"
+
         private val modulationPlayer by lazy {
             ToneModulationPlayer()
         }
 
-        var frequency:Int = 0
-            get() = modulationPlayer.frequency
-            set(value: Int) {
-                field = value
-                modulationPlayer.frequency = value
+        var frequency by Delegates.observable(20) { property, oldValue, newValue ->
+            Log.e(TAG, "Frequency: old = $oldValue, new = $newValue")
+            if(oldValue != newValue) {
+                modulationPlayer.frequency = newValue
             }
+        }
 
-        var enable:Boolean = false
-            get() = modulationPlayer.enable
-            set(value: Boolean) {
-                field = value
-                modulationPlayer.enable = value
+        var enable by Delegates.observable(false) { property, oldValue, newValue ->
+            Log.e(TAG, "Enable: old = $oldValue, new = $newValue")
+            if(oldValue != newValue) {
+                modulationPlayer.enable = newValue
             }
+        }
     }
 
 
@@ -66,16 +72,28 @@ class PlayService: Service() {
             channel
         )
 
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("")
-            .setContentText("").build()
+        val stopIntent = Intent(this, BcReceiver::class.java).apply {
+            action = ACTION_STOP
+            putExtra(EXTRA_NOTIFICATION_ID, 0)
+        }
+        val stopPendingIntent: PendingIntent =
+            PendingIntent.getBroadcast(this, 0, stopIntent, 0)
 
-        startForeground(1, notification)
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Генератор частоты")
+            .setContentText("Генератор низких частот")
+            .setSmallIcon(R.drawable.basin)
+            .addAction(R.drawable.ic_baseline_stop_circle_24, getString(R.string.stop_button),
+                stopPendingIntent)
+            .build()
+
+        startForeground(ONGOING_NOTIFICATION_ID, notification)
     }
 
     override fun onDestroy() {
         stopForeground(false)
         stopSelf()
         super.onDestroy()
+        Log.e(TAG, "onDestroy()")
     }
 }
